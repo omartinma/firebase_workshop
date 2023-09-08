f7eddb0cfffb93c0b149f67b1860c33fedcfbb98
Oscar <martinm.oscar@gmail.com>
2023-09-08 16:43:57 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.firebase_workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_workshop/app/app.dart';
+import 'package:firebase_workshop/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
